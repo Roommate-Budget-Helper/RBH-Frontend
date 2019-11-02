b01aa6d4878c3ac873bc3b20792c5b1d328efef6
Zishan <liuz6@rose-hulman.edu>
2019-11-01 23:36:37 -0400

    add more config

---
+++ Frontend/.prettierrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+    printWidth: 140,
+    tabWidth: 4,
+    useTabs: false,
+    semi: true,
+    singleQuote: true,
+    jsxSingleQuote: false,
+    trailingComma: 'none',
+    bracketSpacing: true,
+    jsxBracketSameLine: false,
+    arrowParens: 'always',
+    rangeStart: 0,
+    rangeEnd: Infinity,
+    requirePragma: false,
+    insertPragma: false,
+    proseWrap: 'preserve',
+    htmlWhitespaceSensitivity: 'css',
+    endOfLine: 'lf'
+};
