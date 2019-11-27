7bd070e3daf30976ff4ef1a7935affc719b0e226
Zishan <liuz6@rose-hulman.edu>
2019-11-26 20:11:52 -0500

    reorganize files + login page

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserModule,
+        ReactiveFormsModule,
+        RouterModule.forRoot([
+            { path: '', component: LoginPageComponent }
+            //{ path: 'products/:productId', component: ProductDetailsComponent },
+        ])
+    ],
+    declarations: [AppComponent, LoginPageComponent],
+    // providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
