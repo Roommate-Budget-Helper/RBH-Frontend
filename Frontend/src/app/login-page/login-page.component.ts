dfa6f885d11cab583fd43e4a009f846b4d41ba6f
Zishan <liuz6@rose-hulman.edu>
2019-12-01 21:27:49 -0500

    fix form

--- Frontend/src/app/login-page/login-page.component.ts
+++ Frontend/src/app/login-page/login-page.component.ts
@@ -1,16 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Location } from '@angular/common';
+import ApiClient from '../api-client';
+
 @Component({
     selector: 'app-login-page',
     templateUrl: './login-page.component.html',
     styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
-    constructor(private location: Location) {}
+    options: any;
+    constructor(private location: Location) {
+        this.options = { username: '', password: '' };
+    }
 
-    ngOnInit() {}
+    ngOnInit = () => {};
 
     handleBack = () => {
         this.location.back();
     };
+
+    handleSubmit = async () => {
+        const result = await ApiClient.auth.login(this.options.username, this.options.password);
+        alert(`${result}`);
+    };
+
+    ngOnDestroy() {}
 }
