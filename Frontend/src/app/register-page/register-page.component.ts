867f93f17190730a769ce144b9907d9ea05eb488
jz <huangjz97@outlook.com>
2019-12-04 14:31:45 -0500

    register finished

--- Frontend/src/app/register-page/register-page.component.ts
+++ Frontend/src/app/register-page/register-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-
+import ApiClient from '../api-client';
+import * as sha256 from 'sha256';
+import * as _ from "lodash";
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html',
@@ -7,11 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterPageComponent implements OnInit {
   options: any;
-  constructor() { this.options = { email:'', username: '', password: '', repassword:'' }}
+  constructor(private router: Router) { this.options = { email:'', username: '', password: '', repassword:'' }}
 
   ngOnInit() {
   }
 
+  handleBack = () => {
+    this.router.navigateByUrl('/');
+  };
 
+  handleSubmit = async () => {
+    const result = await ApiClient.auth.register(this.options.username, sha256(this.options.password),this.options.email);
+    result.isRegistered == false ? alert('user exists') : alert('register succeed');
+  };
 
 }
