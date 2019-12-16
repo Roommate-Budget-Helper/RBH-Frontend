b7ceb69f09eb0e935cc61910598f9ba372368f82
lanxi <568289315wlx@gmail.com>
2019-12-16 16:29:33 -0500

    fix MatDialogRef Issues

--- Frontend/src/app/create-home-page/create-home-page.component.spec.ts
+++ Frontend/src/app/create-home-page/create-home-page.component.spec.ts
@@ -1,6 +1,33 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { RouterTestingModule } from '@angular/router/testing';
+// import { StorageServiceService } from '../storage-service.service';
 import { CreateHomePageComponent } from './create-home-page.component';
+import { FormsModule, ReactiveFormsModule }         from '@angular/forms';
+import { Router } from '@angular/router';
+import {MatDialogRef} from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StorageServiceService } from '../storage-service.service';
+
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatToolbarModule,
+  MatListModule,
+  MatGridListModule,
+  MatCardModule,
+  MatIconModule,
+  MatProgressSpinnerModule,
+  MatMenuModule,
+  MatDialogModule
+  // MatDialogRef
+} from '@angular/material';
 
 describe('CreateHomePageComponent', () => {
   let component: CreateHomePageComponent;
@@ -8,7 +35,27 @@ describe('CreateHomePageComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ CreateHomePageComponent ]
+      declarations: [ CreateHomePageComponent ],
+      imports:[RouterTestingModule,FormsModule,ReactiveFormsModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatSliderModule,
+        MatSlideToggleModule,
+        MatToolbarModule,
+        MatListModule,
+        MatGridListModule,
+        MatCardModule,
+        MatIconModule,
+        MatProgressSpinnerModule,
+        MatMenuModule,
+        MatDialogModule,
+        BrowserAnimationsModule],
+      providers: [{provide : MatDialogRef, useValue : {}}, StorageServiceService],
     })
     .compileComponents();
   }));
--- Frontend/src/app/register-page/register-page.component.spec.ts
+++ Frontend/src/app/register-page/register-page.component.spec.ts
@@ -1,22 +1,61 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { RegisterPageComponent } from './register-page.component';
 import { RouterTestingModule } from '@angular/router/testing';
-// import { Router } from '@angular/router';
-// import {DevExtremeModule} from   '@angular/forms';
+import { Router } from '@angular/router';
 import { FormsModule, ReactiveFormsModule }         from '@angular/forms';
-import {MatPaginatorModule} from '@angular/material';
-import { MatDialogRef } from '@angular/material';
-import { MatDialog } from '@angular/material/dialog';
+// import { MatDialogRef } from '@angular/material';
+// import { MatDialog } from '@angular/material/dialog';
+import {MatDialogRef} from '@angular/material/dialog';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatDatepickerModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatRadioModule,
+  MatSelectModule,
+  MatSliderModule,
+  MatSlideToggleModule,
+  MatToolbarModule,
+  MatListModule,
+  MatGridListModule,
+  MatCardModule,
+  MatIconModule,
+  MatProgressSpinnerModule,
+  MatMenuModule,
+  MatDialogModule
+  // MatDialogRef
+} from '@angular/material';
+
 describe('RegisterPageComponent', () => {
   let component: RegisterPageComponent;
   let fixture: ComponentFixture<RegisterPageComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule,FormsModule,ReactiveFormsModule,MatPaginatorModule],
       declarations: [ RegisterPageComponent],
-      providers: [MatDialogRef]
+      imports: [RouterTestingModule,FormsModule,ReactiveFormsModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatSliderModule,
+        MatSlideToggleModule,
+        MatToolbarModule,
+        MatListModule,
+        MatGridListModule,
+        MatCardModule,
+        MatIconModule,
+        MatProgressSpinnerModule,
+        MatMenuModule,
+        MatDialogModule,
+        BrowserAnimationsModule],
+      providers: [{provide : MatDialogRef, useValue : {}}]
     })
     .compileComponents();
   }));
@@ -38,10 +77,4 @@ describe('RegisterPageComponent', () => {
     expect(compiled.querySelector('h1').textContent).toContain('Sign up');
   });
 
-  // it('should render title in a h1 tag', () => {
-  //   const fixture = TestBed.createComponent(RegisterPageComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.debugElement.nativeElement;
-  //   expect(compiled.querySelector('h1').textContent).toContain('Sign Up');
-  // });
 });
