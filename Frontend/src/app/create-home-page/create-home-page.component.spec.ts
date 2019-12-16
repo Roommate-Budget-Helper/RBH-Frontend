6c4e67f6c39b549ccf459cec09fabaede6b12e58
lanxi <568289315wlx@gmail.com>
2019-12-16 18:15:53 -0500

    small fix

--- Frontend/src/app/create-home-page/create-home-page.component.spec.ts
+++ Frontend/src/app/create-home-page/create-home-page.component.spec.ts
@@ -1,6 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
-// import { StorageServiceService } from '../storage-service.service';
 import { CreateHomePageComponent } from './create-home-page.component';
 import { FormsModule, ReactiveFormsModule }         from '@angular/forms';
 import { Router } from '@angular/router';
