26d6b5b809354b3737d1dc240a55c8b9ae1f4c9b
lanxi <568289315wlx@gmail.com>
2020-01-05 22:07:48 -0500

    Routing Tests for starting page component get worked

--- Frontend/src/app/starting-page/starting-page.component.spec.ts
+++ Frontend/src/app/starting-page/starting-page.component.spec.ts
@@ -1,36 +1,23 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { StartingPageComponent } from './starting-page.component';
-import { RouterTestingModule } from '@angular/router/testing';
 import { Router } from '@angular/router';
-import { Button } from 'protractor';
-import { DebugElement } from '@angular/core';
 import { By } from "@angular/platform-browser";
-import {Component, Directive, Input, ViewChild} from '@angular/core';
-import { LoginPageComponent } from '../login-page/login-page.component';
-import { RegisterPageComponent } from '../register-page/register-page.component';
-import {Location} from '@angular/common'
-import {routes} from '../app.module'
-import { CreateHomePageComponent } from '../create-home-page/create-home-page.component';
-import { HomePageComponent } from '../home-page/home-page.component';
-// import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
+const routerSpy = jasmine.createSpyObj('Router', ['navigateByUrl']);
+
 describe('StartingPageComponent', () => {
-    let componentDe;
     let component: StartingPageComponent;
     let fixture: ComponentFixture<StartingPageComponent>;
-    let location : Location;
     let button;
     let buttons;
-    // .withRoutes([
-    //     { path: 'login', component: LoginPageComponent }])
+
     beforeEach(async(() => {
         TestBed.configureTestingModule({
-            imports: [RouterTestingModule.withRoutes([{path:'', component:StartingPageComponent},{ path: 'login', component: LoginPageComponent}]), FormsModule],
-            declarations: [StartingPageComponent, LoginPageComponent],
-            providers: [ Location, { provide: Router, 
-                useClass: class { navigate = jasmine.createSpy("navigate"); },
-                Component, Directive, Input, ViewChild }]
+            imports:[FormsModule],
+            declarations: [StartingPageComponent],
+            providers: [
+                { provide: Router,   useValue: routerSpy }]
         }).compileComponents();
     }));
 
@@ -84,13 +71,14 @@ describe('StartingPageComponent', () => {
     });
 
     describe('Routing Tests', () => {
-        
+        let router: Router;
         beforeEach(() => {
-            location = TestBed.get(Location);
+            router = fixture.debugElement.injector.get(Router);
             fixture = TestBed.createComponent(StartingPageComponent);
             component = fixture.componentInstance;
-            // componentDe = fixture.debugElement.componentInstance;
             fixture.detectChanges();
+            fixture.whenStable()
+          .then(() => fixture.detectChanges());
         });
 
         it('should call handleRedirectLogin()', () => {
@@ -99,39 +87,47 @@ describe('StartingPageComponent', () => {
             expect(component.handleRedirectLogin).toHaveBeenCalled();
         });
 
-        // it('should call handleRedirectLogin() and redirect to /login', () => {
-        //     spyOn(component, 'handleRedirectLogin');
-        //     component.handleRedirectLogin();
-        //     fixture.whenStable().then(() => {
-        //         // console.log(location.path());
-        //     //    expect(location.path()).toBe('/signup')
-        //         expect(location.path()).toBe('/login');
-        //       });
-        // });
 
-        // it('Login redirect Botton should exist', () => {
-        //     // fixture.detectChanges();
-        //     // fixture.debugElement.query(By.css('.starting-page-buttons'))[0].nativeElement.click();
-        //     button = fixture.debugElement.queryAll(By.css('button'))[0].nativeElement
-        //     expect(button).toBeTruthy();
-        //     fixture.detectChanges();
-        //     button.click()
-        // });
+        it('should call handleRedirectLogin() and redirect to /login', () => {
+            const spy = router.navigateByUrl as jasmine.Spy;
+            component.handleRedirectLogin();
+            fixture.whenStable().then(() => {
+                const navArgs = spy.calls.mostRecent().args[0];
+                expect(navArgs).toBe('/login', 'should nav to login page');
+            });
+        });
 
-        // it('should Login redirect button', async(() => {
-        //     fixture.detectChanges();
-        //     button = fixture.debugElement.queryAll(By.css('button'))[0].nativeElement
-        //     // expect(button.textContent).toContain('Log In');
-        //     // expect(button.textContent).not.toContain('Sign up')
-        //     // console.log(button);
-        //   }));
+        it('should call handleRedirectRegister() and redirect to /register', () => {
+            const spy = router.navigateByUrl as jasmine.Spy;
+            component.handleRedirectRegister();
+            fixture.whenStable().then(() => {
+                const navArgs = spy.calls.mostRecent().args[0];
+                expect(navArgs).toBe('/register', 'should nav to register page');
+            });
+        });
 
-        // it('should call handleRedirectLogin() after the Login button is clicked', () => {
-        //     // let el = fixture.debugElement.queryAll(By.css('.starting-page-buttons'))[0].nativeElement
-        //     button = fixture.debugElement.queryAll(By.css('button'))[0].nativeElement
-        //     fixture.detectChanges();
-        //     button.click()
+        it('Login redirect Botton should exist', () => {
+            const spy = router.navigateByUrl as jasmine.Spy;
+            button = fixture.debugElement.queryAll(By.css('button'))[0].nativeElement
+            expect(button).toBeTruthy();
+            fixture.detectChanges();
+            button.click()
+            fixture.whenStable().then(() => {
+                const navArgs = spy.calls.mostRecent().args[0];
+                expect(navArgs).toBe('/login', 'should nav to login page');
+            });
+        });
 
-        // });
+        it('Register redirect Botton should exist', () => {
+            const spy = router.navigateByUrl as jasmine.Spy;
+            button = fixture.debugElement.queryAll(By.css('button'))[1].nativeElement
+            expect(button).toBeTruthy();
+            fixture.detectChanges();
+            button.click()
+            fixture.whenStable().then(() => {
+                const navArgs = spy.calls.mostRecent().args[0];
+                expect(navArgs).toBe('/register', 'should nav to login page');
+            });
+        });
     });
 });
