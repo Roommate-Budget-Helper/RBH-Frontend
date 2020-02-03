485713eec548fb2961503ab9eca952fa681be28c
jz <huangjz97@outlook.com>
2020-02-02 21:05:41 -0500

    Bill detail functions

--- Frontend/src/app/bill-detail-page/bill-detail-page.component.ts
+++ Frontend/src/app/bill-detail-page/bill-detail-page.component.ts
@@ -9,17 +9,67 @@ const STORAGE_KEY = 'local_userInfo';
     styleUrls: ['./bill-detail-page.component.scss']
 })
 export class BillDetailPageComponent implements OnInit {
-    billDetail: IBill;
+    billDetail: IBillDetail[];
     user = this.StorageService.getLocalStorage(STORAGE_KEY).userInfo;
+    ownerName = '111';
     constructor(private route: ActivatedRoute, private router: Router, private StorageService: StorageServiceService) {}
 
     async ngOnInit() {
         this.billDetail = await ApiClient.bill.getBillById(this.route.snapshot.params['id']);
+        this.findOwner(this.billDetail);
         // this.billDetail = await ApiClient.bill.getBillById('41');
         console.info(this.billDetail);
     }
 
     handleBack = () => {
         this.router.navigateByUrl('/homedetail');
+    };
+
+    findOwner = (billDetail: IBillDetail[]): stringId => {
+        console.info(billDetail);
+        this.billDetail.map((i) => {
+            if (i.ownerId == i.userId) {
+                this.ownerName = i.userName;
+            }
+        });
+        return this.ownerName;
+    };
+
+    getTitle = (index: number): string => {
+        if (this.user.userName === this.ownerName) {
+            //if is owner
+            if (this.billDetail[index].ownerId === this.billDetail[index].userId) {
+                return 'Creator';
+            } else {
+                return 'split';
+            }
+        } else {
+            //if is not owner
+            if (this.billDetail[index].ownerId === this.billDetail[index].userId) {
+                return 'Creator';
+            } else {
+                return 'split';
+            }
+        }
+    };
+
+    getButtonText = (index: number): string => {
+        if (this.user.userName === this.ownerName) {
+            //if is owner
+            if (this.billDetail[index].ownerId === this.billDetail[index].userId) {
+                return 'Upload Recipt';
+            } else {
+                return 'Audit Proof';
+            }
+        } else {
+            //if is not owner
+            if (this.billDetail[index].ownerId === this.billDetail[index].userId) {
+                return 'Check Recipt';
+            } else if (this.user.userId === this.billDetail[index].userId) {
+                return 'Upload Proof';
+            } else {
+                return 'Check Proof';
+            }
+        }
     };
 }
