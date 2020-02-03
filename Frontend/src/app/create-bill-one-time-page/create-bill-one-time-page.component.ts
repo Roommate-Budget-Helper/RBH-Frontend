e6668c7d6f3c79422d70e690db8ca1f41e299248
zisaanlau <zisaanlau@gmail.com>
2020-02-03 16:38:44 -0500

    version

--- Frontend/src/app/create-bill-one-time-page/create-bill-one-time-page.component.ts
+++ Frontend/src/app/create-bill-one-time-page/create-bill-one-time-page.component.ts
@@ -25,15 +25,14 @@ export class CreateBillOneTimePageComponent implements OnInit {
     shareplan_array;
     shareplanId;
     shareplanName;
-    rm_num = this.roommate_array.length-1
+    rm_num = this.roommate_array.length - 1;
     constructor(
         private router: Router,
         public fb: FormBuilder,
         private StorageService: StorageServiceService,
         public dialog: MatDialog,
         public dialogRef: MatDialogRef<any>
-    ) {
-    }
+    ) {}
     oneTimeBillForm = this.fb.group({
         billname: [''],
         description: [''],
@@ -42,37 +41,37 @@ export class CreateBillOneTimePageComponent implements OnInit {
         splitMethod: ['Percentage'],
         addDynamicElement: this.fb.array([])
     });
-    owneram=0;
-    ownerpp:number=100;
-    
+    owneram = 0;
+    ownerpp: number = 100;
+
     async ngOnInit() {
         this.deleteRoommate(this.user.userName);
 
-        await this.getPlan()
+        await this.getPlan();
 
-        console.log(this.shareplan_array)
+        console.log(this.shareplan_array);
     }
 
     updateOwner = () => {
         let result = this.oneTimeBillForm.value;
-        this.owneram=result.amount
-        this.ownerpp=100
+        this.owneram = result.amount;
+        this.ownerpp = 100;
         if (result.splitMethod == 'Amount') {
             this.addDynamicElement.value.forEach((element) => {
                 console.info('element amount: ' + element.amount);
                 // console.info("hhhhhhhhhhh",element.rm_name.rm_name );
-                this.owneram-=element.amount
-                this.ownerpp-=element.amount/result.amount*100
-                this.ownerpp = parseFloat(this.ownerpp.toPrecision(4))
-                console.info(this.ownerpp)
+                this.owneram -= element.amount;
+                this.ownerpp -= (element.amount / result.amount) * 100;
+                this.ownerpp = parseFloat(this.ownerpp.toPrecision(4));
+                console.info(this.ownerpp);
             });
         } else {
             this.addDynamicElement.value.forEach((element) => {
-                this.ownerpp-=parseFloat(element.amount.toPrecision(2))
-                this.owneram-=(element.amount/100*result.amount)
+                this.ownerpp -= parseFloat(element.amount.toPrecision(2));
+                this.owneram -= (element.amount / 100) * result.amount;
             });
         }
-    }
+    };
 
     get shareDetail(): FormGroup {
         return this.fb.group({
@@ -82,15 +81,15 @@ export class CreateBillOneTimePageComponent implements OnInit {
     }
 
     getPlan = async () => {
-        this.shareplan_array = await ApiClient.bill.getSharePlans(this.home.HouseId)
-    }
+        this.shareplan_array = await ApiClient.bill.getSharePlans(this.home.HouseId);
+    };
     handleBack = () => {
         this.router.navigateByUrl('/billoption');
     };
 
     deleteRoommate(msg: string) {
         const index: number = this.roommate_array.indexOf(msg);
-        console.log(index)
+        console.log(index);
         if (index !== -1) {
             this.roommate_array.splice(index, 1);
         }
@@ -119,8 +118,9 @@ export class CreateBillOneTimePageComponent implements OnInit {
         let result = this.oneTimeBillForm.value;
         let result_rm = [];
         let owner_amount, owner_pp;
-        let total_am=0, total_pp=0;
-        
+        let total_am = 0,
+            total_pp = 0;
+
         console.info(this.oneTimeBillForm.controls);
         if (result.splitMethod == 'Amount') {
             this.addDynamicElement.value.forEach((element) => {
@@ -128,32 +128,35 @@ export class CreateBillOneTimePageComponent implements OnInit {
                 // console.info("hhhhhhhhhhh",element.rm_name.rm_name );
                 result_rm.push(element.rm_name);
                 result_am.push(element.amount);
-                total_am+=element.amount
-                result_pp.push(parseFloat((parseFloat(element.amount) / parseFloat(result.amount)*100).toPrecision(2)));
+                total_am += element.amount;
+                result_pp.push(parseFloat(((parseFloat(element.amount) / parseFloat(result.amount)) * 100).toPrecision(2)));
             });
         } else {
             this.addDynamicElement.value.forEach((element) => {
                 console.info('element amount: ' + parseInt(element.amount) * parseInt(result.amount));
                 result_rm.push(element.rm_name);
                 result_am.push(parseInt(element.amount) * parseInt(result.amount));
-                total_am+=parseInt(element.amount) * parseInt(result.amount)/100
-                result_pp.push(parseFloat((element.amount).toPrecision(2)));
+                total_am += (parseInt(element.amount) * parseInt(result.amount)) / 100;
+                result_pp.push(parseFloat(element.amount.toPrecision(2)));
             });
         }
-        result_rm.push(this.user.userName)
-        result_am.push(this.owneram)
-        result_pp.push(this.ownerpp)
+        result_rm.push(this.user.userName);
+        result_am.push(this.owneram);
+        result_pp.push(this.ownerpp);
         // console.info(result)
-        console.info(result_rm)
-        let thisDialogRef = this.dialog.open(SharePlanDialogComponent, {data: { amount:this.owneram, pp:this.ownerpp, spName:this.shareplanName, sp_array:this.shareplan_array }, disableClose: true });
+        console.info(result_rm);
+        let thisDialogRef = this.dialog.open(SharePlanDialogComponent, {
+            data: { amount: this.owneram, pp: this.ownerpp, spName: this.shareplanName, sp_array: this.shareplan_array },
+            disableClose: true
+        });
         let date: Date = new Date();
         thisDialogRef.afterClosed().subscribe(async (res) => {
-            console.info(result)
-            console.info(res)
-            if(res=="back"){
+            console.info(result);
+            console.info(res);
+            if (res == 'back') {
                 return;
             }
-            if (res == "") {
+            if (res == '') {
                 ApiClient.bill.createBill({
                     ownerId: this.user.id,
                     homeId: this.home.HouseId,
@@ -170,7 +173,7 @@ export class CreateBillOneTimePageComponent implements OnInit {
                     created_at: date,
                     created_by: this.user.userName
                 });
-            }else if (this.shareplan_array.indexOf(res)>=0){
+            } else if (this.shareplan_array.indexOf(res) >= 0) {
                 ApiClient.bill.createBill({
                     ownerId: this.user.id,
                     homeId: this.home.HouseId,
@@ -187,8 +190,7 @@ export class CreateBillOneTimePageComponent implements OnInit {
                     created_at: date,
                     created_by: this.user.userName
                 });
-            }
-            else{
+            } else {
                 ApiClient.bill.createBill({
                     ownerId: this.user.id,
                     homeId: this.home.HouseId,
@@ -205,27 +207,22 @@ export class CreateBillOneTimePageComponent implements OnInit {
                     created_at: date,
                     created_by: this.user.userName
                 });
-
             }
-            this.router.navigateByUrl('/homedetail')
-
+            this.router.navigateByUrl('/homedetail');
         });
-        
     }
 
     get addDynamicElement() {
         return this.oneTimeBillForm.get('addDynamicElement') as FormArray;
     }
 
-    
-
     addItems = (value) => {
-        console.info(this.addDynamicElement.controls.length)
-        if (this.roommate_array.length == 0||this.addDynamicElement.controls.length>=this.rm_num) {
+        console.info(this.addDynamicElement.controls.length);
+        if (this.roommate_array.length == 0 || this.addDynamicElement.controls.length >= this.rm_num) {
             alert(`You only have ${this.rm_num} roommates!`);
         } else {
             this.addDynamicElement.push(value);
-            console.info(value)
+            console.info(value);
             this.current_array.push(value);
         }
     };
@@ -236,62 +233,62 @@ export class CreateBillOneTimePageComponent implements OnInit {
     };
 
     updateList = (e) => {
-        console.info("event: ", e.target.value);
-        console.info(e)
-        if(e.target.value==-1){
-            this.shareplanId = -1
-            this.shareplanName = ""
-            while(this.addDynamicElement.length!=0){
-                this.deleteItems()
+        console.info('event: ', e.target.value);
+        console.info(e);
+        if (e.target.value == -1) {
+            this.shareplanId = -1;
+            this.shareplanName = '';
+            while (this.addDynamicElement.length != 0) {
+                this.deleteItems();
             }
-            this.updateOwner()
+            this.updateOwner();
             return;
-        }else{
-            this.shareplanId = this.shareplan_array[e.target.value].id
-            this.shareplanName = this.shareplan_array[e.target.value].full_name
+        } else {
+            this.shareplanId = this.shareplan_array[e.target.value].id;
+            this.shareplanName = this.shareplan_array[e.target.value].full_name;
         }
-        let selectedPlan = this.shareplan_array[e.target.value]
+        let selectedPlan = this.shareplan_array[e.target.value];
         let rm = selectedPlan.userName;
         let pp = selectedPlan.ratio;
-        this.oneTimeBillForm.get("splitMethod").setValue("Percentage")
-        if(this.addDynamicElement.length!=0){
-            while(this.addDynamicElement.length!=0){
-                this.deleteItems()
+        this.oneTimeBillForm.get('splitMethod').setValue('Percentage');
+        if (this.addDynamicElement.length != 0) {
+            while (this.addDynamicElement.length != 0) {
+                this.deleteItems();
             }
         }
-        if(rm.indexOf( this.user.userName)<0){
-            alert("you cannot use this share plan because you are not part of it.")
-        }else{
-            for(let i = 0; i < rm.length;i++){
-                if(rm[i]!=this.user.userName){
-                    
-                        this.addItems(this.fb.group({
+        if (rm.indexOf(this.user.userName) < 0) {
+            alert('you cannot use this share plan because you are not part of it.');
+        } else {
+            for (let i = 0; i < rm.length; i++) {
+                if (rm[i] != this.user.userName) {
+                    this.addItems(
+                        this.fb.group({
                             rm_name: rm[i],
                             amount: pp[i]
-                        }))
-                    
+                        })
+                    );
                 }
             }
-            this.updateOwner()
+            this.updateOwner();
         }
-    }
+    };
 
     changeName(i, e) {
-        let total=this.oneTimeBillForm.value.amount
+        let total = this.oneTimeBillForm.value.amount;
         let evalue = e.target.value;
-        console.log(this.roommate_array.indexOf(evalue))
-        if(this.roommate_array.indexOf(evalue)<0){
-            this.updateOwner()
-            return
+        console.log(this.roommate_array.indexOf(evalue));
+        if (this.roommate_array.indexOf(evalue) < 0) {
+            this.updateOwner();
+            return;
         }
-        console.info(this.addDynamicElement.at(i))
+        console.info(this.addDynamicElement.at(i));
         // this.addDynamicElement.at(i)
         //     .patchValue(
         //         {rm_name: value}
         //     );
         // this.addDynamicElement.get(`${i}`)   .['rm_name'].setValue(value)
         console.info('isisis', evalue);
-        console.info("current: ", this.current_array, i, this.current_array[i] == '')
+        console.info('current: ', this.current_array, i, this.current_array[i] == '');
         // console.info("current array is: "+this.current_array)
         // console.info("roommate array is: "+this.roommate_array)
         // console.info("changed to: "+ value)
@@ -304,7 +301,7 @@ export class CreateBillOneTimePageComponent implements OnInit {
             this.roommate_array.push(this.current_array[i]);
             this.current_array[i] = evalue;
         }
-        console.log(this.current_array, this.roommate_array)
+        console.log(this.current_array, this.roommate_array);
 
         // if(this.oneTimeBillForm.value.splitMethod=="Percentage"){
         //       this.result_pp.push(moneyValue)
