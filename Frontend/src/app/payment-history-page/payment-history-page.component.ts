43e1c6b52d1f0b3478b98115c7c27984558fbb43
jz <huangjz97@outlook.com>
2020-03-22 17:26:38 -0400

    balance finish

--- Frontend/src/app/payment-history-page/payment-history-page.component.ts
+++ Frontend/src/app/payment-history-page/payment-history-page.component.ts
@@ -13,11 +13,13 @@ const HOME_STORAGE_KEY = 'local_homeId';
 //@ts-ignore
 export class PaymentHistoryPageComponent implements OnInit {
     home;
-    roommate_array;
+    roommate_array = [] as string[];
+    balance_array = [] as number[];
     roommate_string = '';
     id;
     user;
     roommates;
+    temp;
     constructor(private router: Router, private StorageService: StorageServiceService) {}
     async ngOnInit() {
         this.user = this.StorageService.getLocalStorage(STORAGE_KEY).userInfo;
@@ -25,9 +27,24 @@ export class PaymentHistoryPageComponent implements OnInit {
         this.roommates = this.StorageService.getHomeLocalStorage(HOME_STORAGE_KEY).roommates;
         this.roommate_string = this.roommates.replace(this.home.admin_name, '');
         this.id = this.StorageService.getHomeLocalStorage(HOME_STORAGE_KEY).id;
+
+        this.temp = await ApiClient.home.getUserbalanceByHome('darcy007', this.home.HouseId);
+
+        this.roommate_array = this.roommates.trim().split('  ');
+
+        this.getBalanceInfo();
     }
 
     handleBack = () => {
         this.router.navigateByUrl('/home');
+    };
+
+    getBalanceInfo = () => {
+        this.roommate_array = this.roommates.trim().split('  ');
+        this.roommate_array.map(async (item, index) => {
+            (await (await ApiClient.home.getUserbalanceByHome(item, this.home.HouseId)).balance) == null
+                ? (this.balance_array[index] = 0)
+                : (this.balance_array[index] = (await ApiClient.home.getUserbalanceByHome(item, this.home.HouseId)).balance);
+        });
     };
 }
