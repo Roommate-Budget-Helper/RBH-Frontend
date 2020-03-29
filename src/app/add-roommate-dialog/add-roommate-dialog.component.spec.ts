0cafa9deb1431799bd68f32fde2668ac5a2ea8c2
lanxi <568289315wlx@gmail.com>
2020-03-29 16:13:38 -0400

    ummm

--- src/app/add-roommate-dialog/add-roommate-dialog.component.spec.ts
+++ src/app/add-roommate-dialog/add-roommate-dialog.component.spec.ts
@@ -15,7 +15,7 @@ describe('AddRoommateDialogComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ AddRoommateDialogComponent ],
-      imports: [MatDialogModule,MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatIconModule, BrowserAnimationsModule, MatAutocompleteModule],
+      imports: [MatDialogModule,MatFormFieldModule, MatInputModule, FormsModule, ReactiveFormsModule, MatIconModule, BrowserAnimationsModule],
       providers:[{provide : MAT_DIALOG_DATA, useValue : {houseId: 1}}]
     })
     .compileComponents();
