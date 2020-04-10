06c80d47d77d19491181329f73e370bc0e79cdfb
jz <huangjz97@outlook.com>
2020-04-10 17:40:15 -0400

    zhe xia jue dui xing

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 //Install express server
-import express, { static } from 'express';
-import { join } from 'path';
+const express = require('express');
+const path = require('path');
 
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(static(__dirname + '/dist/Roommate-Budget-Helper'));
+app.use(express.static(__dirname + '/dist/Roommate-Budget-Helper'));
 
 app.get('/*', function (req, res) {
-    res.sendFile(join(__dirname + '/dist/Roommate-Budget-Helper/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/Roommate-Budget-Helper/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
