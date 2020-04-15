46df72deaeb284a25022bff61e5028f6699ba2e7
Darcy <mr.thank@outlook.com>
2020-04-15 15:12:02 -0400

    bug fix

--- src/app/bill-history-page/bill-history-page.component.ts
+++ src/app/bill-history-page/bill-history-page.component.ts
@@ -10,11 +10,15 @@ import ApiClient from '../api-client';
 })
 export class BillHistoryPageComponent implements OnInit {
     histories;
+    yes=true;
     user = this.StorageService.getLocalStorage(STORAGE_KEY).userInfo;
     constructor(private route: ActivatedRoute, private router: Router, private StorageService: StorageServiceService) {}
 
     async ngOnInit() {
         this.histories = await ApiClient.bill.getBillHistoryById(this.route.snapshot.params['id']);
+        if(this.histories.length==0){
+            this.yes=false;
+        }
         console.info(this.histories)
     }
 
